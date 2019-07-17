e78333dc64916cff7aece31eeb65aaf44db38ba2
Jorge Contreras <jorge.contreras789@gmail.com>
2019-07-17 18:33:09 -0400

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-clean-25283',
-    user: 'contrerasjorge',
-    password: '',
-    database: 'smart-brain'
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
