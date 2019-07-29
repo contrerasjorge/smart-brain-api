d74da4fd45618fcc1e775f02ec6dab4248436461
Jorge Contreras <jorge.contreras789@gmail.com>
2019-07-29 14:03:34 -0400

    updated API key

--- controllers/image.js
+++ controllers/image.js
@@ -1,30 +1,31 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-  apiKey: 'e43b0cd69e8c45128cccea929e52a5e7'
- });
+  apiKey: process.env.CLARIFAI_API
+});
 
- const handleApiCall = (req, res) => {
-  app.models.predict(
-    Clarifai.FACE_DETECT_MODEL, req.body.input)
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API'))
- }
+    .catch(err => res.status(400).json('unable to work with API'));
+};
 
 const handleImage = (req, res, db) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
+  const { id } = req.body;
+  db('users')
+    .where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
     .then(entries => {
       res.json(entries[0]);
     })
-    .catch(err => res.status(400).json('unable to get entries'))
-  }
+    .catch(err => res.status(400).json('unable to get entries'));
+};
 
 module.exports = {
   handleImage,
   handleApiCall
-}
+};
